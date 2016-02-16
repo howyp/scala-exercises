fe65052ed28c6664b7842becf556527882c29151
Howy Perrin <howard.perrin@digital.hmrc.gov.uk>
2016-02-16 12:53:30 +0000

    complete queue impl

--- src/main/scala/scalaexercises/queue.scala
+++ src/main/scala/scalaexercises/queue.scala
@@ -20,17 +20,22 @@ sealed trait Queue[T] {
 object Queue {
   // It would be preferable to implement Empty as a case object to allow pattern matching,
   // but then T would need to be defined as +T
-  class Empty[T] extends Queue[T] {
+  private class Empty[T] extends Queue[T] {
     val isEmpty = true
-    def insert(t: T) = NonEmpty[T](List(t), List.empty)
+    def insert(t: T) = NonEmpty[T](front = List(t), back = List.empty)
     def head = None
     def tail = throw new RuntimeException("tail of an empty queue")
   }
-  case class NonEmpty[T](front: List[T], back: List[T]) extends Queue[T] {
+  private case class NonEmpty[T](back: List[T], front: List[T]) extends Queue[T] {
+    require(!front.isEmpty)
     val isEmpty = false
-    def insert(t: T) = ???
-    def head = front.headOption
-    def tail = new Empty[T]
+    def insert(t: T) = NonEmpty[T](back = t :: back, front = front)
+    def head = Some(front.head)
+    def tail = (back, front.tail) match {
+      case (Nil,          Nil           ) => empty
+      case (nonEmptyBack, Nil           ) => NonEmpty(back = List.empty,   front = back.reverse)
+      case (nonEmptyBack, remainingFront) => NonEmpty(back = nonEmptyBack, front = remainingFront)
+    }
   }
   def empty[T]: Queue[T] = new Empty[T]
 }
--- src/test/scala/scalaexercises/QueueSpec.scala
+++ src/test/scala/scalaexercises/QueueSpec.scala
@@ -5,16 +5,25 @@ import org.scalatest._
 class QueueSpec extends FreeSpec with Matchers {
   "A Queue" - {
     val emptyQueue = Queue.empty[String]
+    val firstValue = "example value"
+    val singleItemQueue = emptyQueue.insert(firstValue)
+    val secondValue = "another value"
+    val twoItemQueue = singleItemQueue.insert(secondValue)
     "an empty queue should" - {
-      "be empty" in { emptyQueue.isEmpty should be (true) }
-      "have no head" in { emptyQueue.head should be (None) }
+      "be empty" in                   { emptyQueue.isEmpty should be (true) }
+      "have no head" in               { emptyQueue.head should be (None) }
       "throw an exception on tail" in { an [Exception] should be thrownBy emptyQueue.tail }
     }
     "after inserting an element, a queue should" - {
-      val singleItemQueue = emptyQueue.insert("example value")
-      "not be empty" in { singleItemQueue.isEmpty should be (false) }
-      "have a head of the inserted element" in { singleItemQueue.head should contain ("example value") }
-      "have an empty tail" in { singleItemQueue.tail.isEmpty should be (true) }
+      "not be empty" in                        { singleItemQueue.isEmpty should be (false) }
+      "have a head of the inserted element" in { singleItemQueue.head should contain (firstValue) }
+      "have an empty tail" in                  { singleItemQueue.tail.isEmpty should be (true) }
+    }
+    "adding further elements to the list should" - {
+      "stil be empty" in                           { twoItemQueue.isEmpty should be (false) }
+      "still have the old head" in                 { twoItemQueue.head should contain (firstValue) }
+      "put the new element in the tail" in         { twoItemQueue.tail.head should contain (secondValue) }
+      "end up being empty if tail called twice" in { twoItemQueue.tail.tail.isEmpty should be (true) }
     }
   }
 }
