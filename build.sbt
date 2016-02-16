4b46be4203662a4cff14245e29ac37b93fb63fa9
Howy Perrin <howard.perrin@digital.hmrc.gov.uk>
2016-02-16 22:37:42 +0000

    Add property test for Queue

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
 lazy val scalaexercises = (project in file(".")).settings(scalaVersion := "2.11.5")
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.12.5" % "test"
--- src/test/scala/scalaexercises/QueueSpec.scala
+++ src/test/scala/scalaexercises/QueueSpec.scala
@@ -1,8 +1,10 @@
 package scalaexercises
 
+import org.scalacheck.Gen
 import org.scalatest._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
-class QueueSpec extends FreeSpec with Matchers {
+class QueueSpec extends FreeSpec with Matchers with GeneratorDrivenPropertyChecks {
   "A Queue" - {
     val emptyQueue = Queue.empty[String]
     val firstValue = "example value"
@@ -24,6 +26,29 @@ class QueueSpec extends FreeSpec with Matchers {
       "still have the old head" in                 { twoItemQueue.head should contain (firstValue) }
       "put the new element in the tail" in         { twoItemQueue.tail.head should contain (secondValue) }
       "end up being empty if tail called twice" in { twoItemQueue.tail.tail.isEmpty should be (true) }
+    }
+    "elements should always be removed in the order they are inserted" in {
+      trait Command
+      case object Insert extends Command
+      case object Remove extends Command
+
+      forAll(Gen.listOf(Gen.oneOf(Insert, Remove))) { commandList: List[Command] =>
+        var lastInserted = 0
+        var lastRemoved = 0
+        var queue = Queue.empty[Int]
+        commandList foreach {
+          case Insert =>
+            lastInserted = lastInserted + 1
+            queue = queue.insert(lastInserted)
+          case Remove =>
+            if (lastInserted == lastRemoved) queue.head should be (None)
+            else {
+              lastRemoved = lastRemoved + 1
+              queue.head should contain (lastRemoved)
+              queue = queue.tail
+            }
+        }
+      }
     }
   }
 }
